1185ad853a3eef7eccbc0389b473f5d8f90f12e1
papaurke <urosmiric88@gmail.com>
2023-06-20 17:17:08 +0200

    Gpt able to continue the story

--- pages/api/gpt.ts
+++ pages/api/gpt.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  data: any
-  message: string
-}
+  data: any;
+  message: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,20 +13,50 @@ export default async function handler(
   const headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.GPT_API_KEY}`,
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
+  console.log(req.body);
+  let body;
 
-  const settingsForPrompt = "with at least five levels in json format like this { story: string; choices: { text: string; story: Story; }[]; }.";
+  if (req.body) {
+    body = JSON.parse(req.body);
+  }
 
-  let basePrompt = `Give me a textual game story with two choices on each level about a cowboy fighting aliens and using his gun ${settingsForPrompt}}`;
+  const { choice, story } = body || {};
+
+  console.log("choice", choice);
+  console.log("story", story);
+
+  let firstPartOfPrompt = `Continue my story ${story} and give me next level of textual game story with two choices. My choice was ${choice}`;
+
+  if (!choice || !story) {
+    const stories = [
+      "me as a a knight trying to sell carrots to a dragon",
+      "me as a banana sold cheap at market, and I am trying to survive in new home with my new friend apple",
+      "me as a cyber cowboy fighting the aliens",
+      "me as a teardrop in the ocean, trying to be a king of the sea",
+      "me as a cat trying to find my way home",
+      "me as a dragon trying to find love",
+      "me as a barbarian which is trying to find a way to start sewing",
+    ];
+
+    firstPartOfPrompt =
+      "Give me the first level of textual game story with two choices about";
+    const gameStory = stories[Math.floor(Math.random() * stories.length)];
+    firstPartOfPrompt = `${firstPartOfPrompt} ${gameStory}`;
+    console.log("gameStory", gameStory);
+  }
+
+  const settingsForPrompt =
+    "in json format like this { story: string; choices: string[]; }.";
+
+  let basePrompt = `${firstPartOfPrompt} ${settingsForPrompt}}`;
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers,
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [
-        { role: "user", content: basePrompt }
-      ],
+      messages: [{ role: "user", content: basePrompt }],
       temperature: 0.8,
       max_tokens: 1024,
     }),
@@ -34,9 +64,12 @@ export default async function handler(
 
   const data = await response.json();
 
-  console.log(data.choices);
+  if (data.choices[0].finish_reason === "length") {
+    console.log("Too long prompt");
+  }
+
   res.status(200).json({
-    message: 'success',
+    message: "success",
     data: data.choices[0],
-  })
+  });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 import ButtonBadge from "@/components/ButtonBadge/ButtonBadge";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-type Choice = {
-  text: string;
-  story: Story;
-};
-
 type Story = {
   story: string;
-  choices: Choice[];
+  choices: string[];
 };
 
 export default function Home() {
@@ -28,55 +23,47 @@ export default function Home() {
       method: "POST",
     });
     const result = await chatGptData.json();
-    const storyData = await JSON.parse(result.data.message.content);
-    setStory(storyData);
+    const firstLevel = await JSON.parse(result.data.message.content);
+    setStory(firstLevel);
     setLoading(false);
-    console.log(storyData);
+    console.log(firstLevel);
   };
+
+  const getNextLevel = async (choice: string, story:string) => {
+    console.log(story);
+    const chatGptData = await fetch("/api/gpt", {
+      method: "POST",
+      body: JSON.stringify({ choice, story }),
+    });
+    const result = await chatGptData.json();
+    const nextLevel = await JSON.parse(result.data.message.content);
+    setStory(nextLevel);
+    console.log(nextLevel);
+  }
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-12 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
       <div className="flex flex-col items-center">
-        {!story.story && (
-          <>
-            <h1 className="font-mono">Textual Games</h1>
-            <div className="mt-20 flex justify-center">
-              <ButtonBadge content="Cowboy Adventure" onClick={getData} />
-              <ButtonBadge
-                content="The Knight"
-                color="#A2B59F"
-                onClick={getData}
-              />
-              <ButtonBadge
-                content="Superheroes"
-                color="#E2D2D2"
-                onClick={getData}
-              />
-            </div>
-          </>
-        )}
-
-        {loading && (
-          <div className="flex justify-center mt-10">
-            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-          </div>
-        )}
-
-        <div>
+        <div className="grid h-screen place-items-center">
+          {!story.story && !loading && (
+            <ButtonBadge content="Play The Game" onClick={getData} />
+          )}
+          {loading && (
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-500"></div>
+          )}
           {story.story && (
-            <div className="px-20">
+            <div className="flex-row justify-center text-center max-w-6xl">
               <p className="text-2xl mt-10 mb-5">{story.story}</p>
-              {story.choices &&
-                story.choices.map((choice: Choice) => (
-                  <div className="flex justify-center" key={choice.text}>
-                    <ButtonBadge
-                      content={choice.text}
-                      onClick={() => setStory(choice.story)}
-                    />
-                  </div>
-                ))}
+              {story.choices && story.choices.map((choice: string, index: number) => (
+                <div className="flex justify-center" key={index}>
+                  <ButtonBadge
+                    content={choice}
+                    onClick={() => getNextLevel(choice, story.story)}
+                  />
+                </div>
+              ))}
             </div>
           )}
         </div>
