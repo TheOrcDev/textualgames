43b806fad10f74f975b825fbdb849d1636dfdff2
papaurke <urosmiric88@gmail.com>
2023-06-23 23:30:02 +0200

    White font color

--- components/ButtonBadge/ButtonBadge.tsx
+++ components/ButtonBadge/ButtonBadge.tsx
@@ -13,7 +13,7 @@ export const ButtonBadge: React.FC<ButtonBadgeProps> = ({
     <button
       style={{ backgroundColor: color }}
       className="
-      transition ease-in-out delay-150 hover:-translate-y-1 p-3 hover:shadow-2xl h-28
+      transition ease-in-out delay-150 hover:-translate-y-1 p-3 hover:shadow-2xl min-h-28
       m-2 flex text-center w-full rounded shadow-md bg-teal-600 text-xs md:text-base
       hover:scale-110 duration-300 items-center justify-center text-white shadow-gray-700
       "
