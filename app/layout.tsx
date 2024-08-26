de3f65927a05afcaca3bba81e603726b8aeb8983
papaurke <urosmiric88@gmail.com>
2024-08-26 16:02:12 +0200

    feat(app): add NextAuth
    authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { Footer, ThemeProvider } from "@/components/ui";
 import { GoogleAnalytics } from "@/components/entities";
 
 import { cn } from "@/lib/utils";
+import NextAuthProvider from "@/components/providers/next-auth-provider";
+import { getSession } from "@/lib/auth";
 
 const pressStart2P = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
@@ -15,24 +17,28 @@ export const metadata: Metadata = {
   description: "Create your own story! AI Generated Textual Games.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getSession();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <GoogleAnalytics />
       <body className={cn(pressStart2P.className)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Footer />
-        </ThemeProvider>
+        <NextAuthProvider session={session}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
