00e9c9ff7adb485013b846a09cce398bb697cd44
papaurke <urosmiric88@gmail.com>
2024-09-04 16:31:53 +0200

    fix(pages): change height of landing page to 100 h-svh
    full height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui";
 
 export default function PlayPage() {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2]">
+    <div className="relative flex h-svh w-full flex-col items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2]">
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-center text-4xl font-bold text-transparent sm:text-7xl">
         Textual Games
