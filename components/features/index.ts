32ef798e54ee3a841897a48081ce4e091649fb8c
papaurke <urosmiric88@gmail.com>
2024-09-04 17:23:02 +0200

    refactor(my-games): move entire client side to a separated component
    client vs server

--- components/features/index.ts
+++ components/features/index.ts
@@ -4,6 +4,7 @@ export { default as GoogleAnalytics } from "./google-analytics/google-analytics"
 export { default as Genres } from "./level-selections/genres/genres";
 export { default as SelectItems } from "./level-selections/select-items/select-items";
 export { default as LoadingSentences } from "./loading-sentences/loading-sentences";
+export { default as MyGames } from "./my-games/my-games";
 export { default as NotEnoughTokens } from "./not-enough-tokens/not-enough-tokens";
 export { default as PaymentSuccess } from "./payment-success/payment-success";
 export { default as UserInfo } from "./user/user-info";
