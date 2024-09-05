cc85fd9bf5b76cfc181fd7b46b5b22fafde2e063
papaurke <urosmiric88@gmail.com>
2024-09-05 18:25:05 +0200

    fix(game): remove console log from api call
    fix

--- server/routers/games.ts
+++ server/routers/games.ts
@@ -32,8 +32,6 @@ export const gamesRouter = router({
           where: eq(games.id, input.gameId),
         });
 
-        console.log(game);
-
         return game;
       } catch (error) {
         console.log(error);
