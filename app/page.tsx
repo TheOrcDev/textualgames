c8a8bcf7acd934aaf6bc9bbcd997046aec8dadac
papaurke <urosmiric88@gmail.com>
2024-09-06 18:15:51 +0200

    feat(page.tsx): add beta badge on landing page
    beta

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
-import { Button } from "@/components/ui";
+import { Badge, Button } from "@/components/ui";
 
 export default function PlayPage() {
   return (
-    <div className="relative flex h-svh w-full flex-col items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black dark:bg-grid-white/[0.2]">
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+    <div className="relative flex h-svh w-full flex-col items-center justify-center gap-1 bg-background bg-grid-black/[0.2] dark:bg-grid-white/[0.2]">
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-center text-4xl font-bold text-transparent sm:text-7xl">
         Textual Games
       </p>
+      <Badge>beta</Badge>
       <Link href={"/play"}>
         <Button>Play Your Story</Button>
       </Link>
