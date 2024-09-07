15432c3306496ff8a7a26b3489b8d4227cfe6b34
papaurke <urosmiric88@gmail.com>
2024-09-07 23:29:23 +0200

    refactor(game): removed unnecessary code
    readability

--- components/features/game/game.tsx
+++ components/features/game/game.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { StoryLevel } from "@/components/features";
-import { Genre, type Game as GameType } from "@/components/shared/types";
 import { Skeleton } from "@/components/ui";
 import { trpc } from "@/server/client";
 
@@ -20,36 +19,20 @@ export default function Game({ gameId }: Props) {
     return <NoGame />;
   }
 
-  if (game.data) {
-    const gameData: GameType = {
-      ...game.data,
-      character: game?.data?.character ?? {
-        type: "",
-        id: "",
-        name: "",
-        createdAt: "",
-        plot: "",
-        items: "",
-        gameId: "",
-      },
-      genre: Genre.SCIFI,
-    };
-
-    return (
-      <>
-        {game.isPending && (
-          <div className="flex items-center justify-center">
-            <div className="grid gap-3 md:grid-cols-2">
-              <Skeleton className="size-72" />
-              <Skeleton className="size-72" />
-              <Skeleton className="hidden size-72 md:block" />
-              <Skeleton className="hidden size-72 md:block" />
-            </div>
+  return (
+    <>
+      {game.isPending && (
+        <div className="flex items-center justify-center">
+          <div className="grid gap-3 md:grid-cols-2">
+            <Skeleton className="size-72" />
+            <Skeleton className="size-72" />
+            <Skeleton className="hidden size-72 md:block" />
+            <Skeleton className="hidden size-72 md:block" />
           </div>
-        )}
+        </div>
+      )}
 
-        {game.data && <StoryLevel game={gameData} />}
-      </>
-    );
-  }
+      {game.data && <StoryLevel game={game.data} />}
+    </>
+  );
 }
