8b43246f190d7bba5744fa8d6c0ee779e8366f44
OrcDev <urosmiric88@gmail.com>
2025-01-29 13:44:48 +0100

    feat(landing page) add spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { GlowEffect } from "@/components/ui/glow-effect";
+import { Spotlight } from "@/components/ui/spotlight";
 
-export default function PlayPage() {
+export default function Homepage() {
   return (
-    <div className="relative flex h-svh w-full flex-col items-center justify-center gap-1 bg-background bg-grid-black/[0.2] dark:bg-grid-white/[0.2]">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center gap-1 bg-background p-24 bg-grid-black/[0.2] dark:bg-grid-white/[0.05]">
+      <Spotlight />
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <h1 className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-center text-4xl font-bold text-transparent sm:text-7xl">
         Textual Games
@@ -18,9 +20,7 @@ export default function PlayPage() {
             duration={3}
             scale={0.9}
           />
-          <Button className="relative inline-flex items-center gap-1 rounded-md bg-zinc-950 px-2.5 py-1.5 text-sm text-zinc-50 outline outline-1 outline-[#fff2f21f]">
-            Try For Free
-          </Button>
+          <Button className="relative bg-zinc-950">Try For Free</Button>
         </div>
       </Link>
     </div>
