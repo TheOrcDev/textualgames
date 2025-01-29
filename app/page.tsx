e16d8ba2997b80e21a6e6e4a7fdba70a5c4218a8
OrcDev <urosmiric88@gmail.com>
2025-01-29 17:23:08 +0100

    feat(landing page) make spotlight bigger

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { Spotlight } from "@/components/ui/spotlight";
 
 export default function Homepage() {
   return (
-    <main className="relative flex h-svh w-full flex-col overflow-hidden rounded-md bg-black/[0.96] antialiased bg-grid-white/[0.02] md:items-center md:justify-center">
-      <Spotlight />
+    <div className="relative flex h-svh w-full flex-col overflow-hidden rounded-md bg-black/[0.96] antialiased bg-grid-white/[0.02] md:items-center md:justify-center">
+      <Spotlight height={1500} />
       <h1 className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-center text-3xl font-bold text-transparent md:text-4xl xl:text-7xl">
         Textual Games
       </h1>
@@ -22,6 +22,6 @@ export default function Homepage() {
           <Button className="relative bg-zinc-950">Try For Free</Button>
         </div>
       </Link>
-    </main>
+    </div>
   );
 }
