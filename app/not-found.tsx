553ad80659d63a7092dfb19f297732282a45dcd0
OrcDev <urosmiric88@gmail.com>
2025-01-29 13:19:04 +0100

    feat(404) add new image, and change button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { GlowEffect } from "@/components/ui/glow-effect";
 
 export const metadata: Metadata = {
   title: "404 | Textual Games",
@@ -12,7 +13,7 @@ export default function NotFound() {
   return (
     <div className="grid h-screen w-full place-content-center gap-5 bg-background px-4 text-center">
       <Image
-        src={"/img/logo/textual-games-logo.png"}
+        src={"/img/404/pixel.webp"}
         width={300}
         height={300}
         alt="Textual Games 404"
@@ -23,7 +24,17 @@ export default function NotFound() {
 
       <p className="text-gray-500">You are lost.</p>
       <Link href={"/"}>
-        <Button variant={"outline"}>Return to Home Page</Button>
+        <div className="relative">
+          <GlowEffect
+            colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
+            mode="colorShift"
+            duration={3}
+            scale={0.9}
+          />
+          <Button className="relative inline-flex items-center gap-1 rounded-md bg-zinc-950 px-2.5 py-1.5 text-sm text-zinc-50 outline outline-1 outline-[#fff2f21f]">
+            Return to Home Page
+          </Button>
+        </div>
       </Link>
     </div>
   );
