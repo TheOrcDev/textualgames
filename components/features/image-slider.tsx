9c44d58d3c8a9e1d0a592658828c5e9a054c1515
OrcDev <urosmiric88@gmail.com>
2025-01-30 00:25:47 +0100

    feat(image-slider) add alts for each image

--- components/features/image-slider.tsx
+++ components/features/image-slider.tsx
@@ -3,12 +3,30 @@ import Image from "next/image";
 import { InfiniteSlider } from "../ui/infinite-slider";
 
 const images = [
-  "/img/screenshots/demon-leading-an-army.webp",
-  "/img/screenshots/orc-leading-an-army.webp",
-  "/img/screenshots/zombie-trying-to-escape-from-magical-realm.webp",
-  "/img/screenshots/resistance-leader-dystopian.webp",
-  "/img/screenshots/alien-post-apocalyptic.webp",
-  "/img/screenshots/space-explorer-in-space.webp",
+  {
+    src: "/img/screenshots/demon-leading-an-army.webp",
+    alt: "Demon leading an army",
+  },
+  {
+    src: "/img/screenshots/orc-leading-an-army.webp",
+    alt: "Orc leading an army",
+  },
+  {
+    src: "/img/screenshots/zombie-trying-to-escape-from-magical-realm.webp",
+    alt: "Zombie trying to escape from magical realm",
+  },
+  {
+    src: "/img/screenshots/resistance-leader-dystopian.webp",
+    alt: "Resistance leader dystopian",
+  },
+  {
+    src: "/img/screenshots/alien-post-apocalyptic.webp",
+    alt: "Alien post apocalyptic",
+  },
+  {
+    src: "/img/screenshots/space-explorer-in-space.webp",
+    alt: "Space explorer in space",
+  },
 ];
 
 export default function ImageSlider() {
@@ -16,9 +34,9 @@ export default function ImageSlider() {
     <InfiniteSlider duration={75} gap={24}>
       {images.map((image) => (
         <Image
-          src={image}
-          key={image}
-          alt="Dean blunt - Black Metal 2"
+          src={image.src}
+          key={image.src}
+          alt={image.alt}
           className="aspect-video w-96 rounded-md md:w-full"
           width={500}
           height={500}
