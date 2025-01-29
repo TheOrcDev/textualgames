5b6d70315d4a24f351303f1cbb1cb8adc7f9cec2
OrcDev <urosmiric88@gmail.com>
2025-01-29 23:13:48 +0100

    fix(landing-page) remove spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Homepage() {
               src={image}
               key={image}
               alt="Dean blunt - Black Metal 2"
-              className="rounded-md"
+              className="w-96 rounded-md md:w-full"
               width={500}
               height={500}
             />
