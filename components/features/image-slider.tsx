2a4bf612f2365d2175b267e69cad807c5c2b831e
OrcDev <urosmiric88@gmail.com>
2025-02-01 00:47:15 +0100

    feat(image-slider) add new djin image

--- components/features/image-slider.tsx
+++ components/features/image-slider.tsx
@@ -4,16 +4,16 @@ import { InfiniteSlider } from "../ui/infinite-slider";
 
 const images = [
   {
-    src: "/img/screenshots/demon-leading-an-army.webp",
-    alt: "Demon leading an army",
-  },
-  {
     src: "/img/screenshots/orc-leading-an-army.webp",
     alt: "Orc leading an army",
   },
   {
     src: "/img/screenshots/zombie-trying-to-escape-from-magical-realm.webp",
     alt: "Zombie trying to escape from magical realm",
+  },
+  {
+    src: "/img/screenshots/demon-leading-an-army.webp",
+    alt: "Demon leading an army",
   },
   {
     src: "/img/screenshots/resistance-leader-dystopian.webp",
@@ -34,6 +34,10 @@ const images = [
   {
     src: "/img/screenshots/orc-with-potion-of-strength.webp",
     alt: "Orc with potion of strength",
+  },
+  {
+    src: "/img/screenshots/djin-finding-lost-soul.webp",
+    alt: "Djin finding lost soul",
   },
 ];
 
