277e323b80ee7ce61b5793e59b1e30b2818dcabb
OrcDev <urosmiric88@gmail.com>
2025-02-01 23:01:23 +0100

    feat(start-playing) title text to xl on mobile devices

--- components/sections/start-playing.tsx
+++ components/sections/start-playing.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 export default function StartPlaying() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 font-mono">
-      <h3 className="text-center text-2xl font-bold">
+      <h3 className="text-center text-xl font-bold md:text-2xl">
         Are you ready to explore Textual Games?
       </h3>
       <div className="relative flex h-40 w-full max-w-xl items-center justify-center p-12 bg-dot-black/[0.2] dark:bg-dot-white/[0.2] ">
