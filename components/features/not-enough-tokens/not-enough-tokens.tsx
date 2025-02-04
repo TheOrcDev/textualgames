a4de75cd609b3e65323cf901417b8b86d9d7c00d
OrcDev <urosmiric88@gmail.com>
2025-02-04 22:39:15 +0100

    feat(not-enough-tokens) make it look better

--- components/features/not-enough-tokens/not-enough-tokens.tsx
+++ components/features/not-enough-tokens/not-enough-tokens.tsx
@@ -10,19 +10,24 @@ import {
 } from "@/components/ui/card";
 export default function NotEnoughTokens() {
   return (
-    <Card className="min-w-96">
-      <CardHeader>
-        <CardTitle>Not enough tokens :(</CardTitle>
-        <CardDescription>You need at least one token to play.</CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-5">
-        <p>
-          Ready to unlock more? Get tokens now and continue playing your story.
-        </p>
-        <Link href={"/buy-tokens"}>
-          <Button>Buy Now!</Button>
-        </Link>
-      </CardContent>
-    </Card>
+    <div className="flex flex-col items-center justify-center gap-5">
+      <Card className="min-w-96">
+        <CardHeader>
+          <CardTitle>Not enough tokens :(</CardTitle>
+          <CardDescription>
+            You need at least one token to play.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-5">
+          <p>
+            Ready to unlock more? Get tokens now and continue playing your
+            story.
+          </p>
+          <Link href={"/buy-tokens"}>
+            <Button>Buy Now!</Button>
+          </Link>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
