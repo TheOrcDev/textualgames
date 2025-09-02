82a41ef9b5c559160ade0c2189c4a06e35a3feda
OrcDev <urosmiric88@gmail.com>
2025-09-02 22:55:34 +0200

    refactor(features): remove UserInfo export from features index

--- components/features/index.ts
+++ components/features/index.ts
@@ -1,4 +1,3 @@
 export { default as GameCard } from "./game/game-card";
 export { default as LoadingSentences } from "./loading-sentences";
-export { default as UserInfo } from "./user/user-info";
 
