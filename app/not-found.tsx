ce677b320de1f3f0340c5a406f8fe920dc33f609
OrcDev <urosmiric88@gmail.com>
2025-09-04 00:57:11 +0200

    refactor(not-found): update button import and simplify layout by removing GlowEffect component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,8 +2,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
-import { GlowEffect } from "@/components/ui/glow-effect";
+import { Button } from "@/components/ui/8bit/button";
 
 export const metadata: Metadata = {
   title: "404 | Textual Games",
@@ -24,22 +23,11 @@ export default function NotFound() {
 
       <h1 className="text-2xl font-bold tracking-tight sm:text-4xl">Uh-oh!</h1>
 
-      <p className="text-gray-500">You are lost.</p>
+      <p>You are lost.</p>
       <div className="flex justify-center">
-        <div className="relative w-max">
-          <GlowEffect
-            colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
-            mode="colorShift"
-            duration={3}
-            scale={0.9}
-          />
-          <Button
-            className="relative inline-flex items-center gap-1 rounded-md bg-zinc-950 px-2.5 py-1.5 text-sm text-zinc-50 outline outline-[#fff2f21f]"
-            asChild
-          >
-            <Link href={"/"}>Return to Home Page</Link>
-          </Button>
-        </div>
+        <Button asChild>
+          <Link href={"/"}>Return to Home Page</Link>
+        </Button>
       </div>
     </div>
   );
